03bcc1c1a9fa2c59897362c9d04fb46a3bd2c007
Adelicult <57094973+Adelicult@users.noreply.github.com>
2019-10-28 16:00:14 +0300

    Add files via upload

---
+++ Program 1.1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number");
+            double number = Convert.ToDouble(Console.ReadLine());
+            number = number * 10;
+            int result = (int)number % 10;
+            Console.WriteLine("Result = " + result);
+            Console.ReadKey();
+        }
+
+    }
+    }
+}
